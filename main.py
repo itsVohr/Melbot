2ae196a7371915678fd778f8a68918ee1b3c43f6
itsVohr <itsvohr@proton.me>
2024-06-28 13:31:55 +0200

    add basic code to run the bot

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import discord
 import os
 import dotenv
+from discord.ext import commands
 
 dotenv.load_dotenv()
+
+# bot initial setup
+intents = discord.Intents.default()
+intents.message_content = True
 discord_token = os.environ['DISCORD_TOKEN']
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# bot events
+#TODO
+
+# run the bot
+bot.run(discord_token)
